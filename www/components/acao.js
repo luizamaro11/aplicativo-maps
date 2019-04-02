0889b0ebba44262d3b1202e5ff2a844a1726d69c
vlvkvinicius <lourenco_vinicius@yahoo.com>
2019-04-03 03:35:31 +0900

    css e inicio js

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
+function retorno(){
+  navigator.vibrate(6000)
+}
+$(document).on('click','#mapa',function(){
+  function checkConnection() {
+    var networkState = navigator.connection.type;
 
+    var states = {};
+    states[Connection.NONE] = 'sem conexão, por favor conectar a rede!';
+    document.addEventListener("offline", retorno, true);
+
+    alert( states[networkState]);
+}
+  checkConnection();
+})
