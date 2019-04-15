2fc64122f96e6372924f4a56ac843f77107b736f
luizamaro11 <luiz.amaro90@gmail.com>
2019-04-16 04:06:31 +0900

    fazendo a geolocalização.

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,5 +1,7 @@
 // This is a JavaScript file
 $(document).on('click','#mapa',function(){
+
+  // functiion que verificará a conexao da internet se não tiver irá gerar 3 beep e vibrará por 6 segundos
   function checkConnection() {
     var networkState = navigator.connection.type;
   
@@ -10,10 +12,38 @@ $(document).on('click','#mapa',function(){
       navigator.notification.beep(3);
       alert(states[networkState]);      
       }
-    
-    
-    
-}
+    else{
+
+    }
+  }
     checkConnection();
-  
-})
+  // o programa nesse momento irá pegar a localização atual do dispositivo e mostrará a localização
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+    var watchID = null;
+
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        var options = { timeout: 30000 };
+        watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    }
+
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />' +
+                            '<hr />'      + element.innerHTML;
+    }
+
+    function clearWatch() {
+        if (watchID != null) {
+            navigator.geolocation.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+    }
+}); 
